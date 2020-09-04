f56cd26d7d12068e36b13d80d4642d75c3875933
Ravi Trivedi <travi@redhat.com>
2020-09-04 15:31:22 +0530

    Adding operator-sdk files as per 1.0.0 version of operator-sdk

--- pkg/apis/rbt/group.go
+++
@@ -1,6 +0,0 @@
-// Package rbt contains rbt API versions.
-//
-// This file ensures Go source parsers acknowledge the rbt package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package rbt
