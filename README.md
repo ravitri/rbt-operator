f0d5ca726661560f80eb47da6313e5446277f995
Ravi Trivedi <travi@redhat.com>
2020-10-01 15:59:03 +0530

    Cleaning repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# Sample Operator rbt-operator
--- scripts/cleanup.sh
+++
@@ -1,7 +0,0 @@
-# Cleanup
-oc delete -f deploy/crds/rbt.redhat.com_v1alpha1_appservice_cr.yaml
-oc delete -f deploy/operator.yaml
-oc delete -f deploy/role.yaml
-oc delete -f deploy/role_binding.yaml
-oc delete -f deploy/service_account.yaml
-oc delete -f deploy/crds/rbt.redhat.com_appservices_crd.yaml
